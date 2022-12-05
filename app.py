23fce914828b76572fb62d2eef195979d8286fed
Jorge Calvar <jorge.calvar@hotmail.com>
2022-12-05 23:33:34 +0100

    Added bootstrap

--- app.py
+++ app.py
@@ -2,17 +2,53 @@
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 import plotly.graph_objects as go
 import plotly.express as px
 
 import pandas as pd
 
+
+load_figure_template('yeti')
+
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+    external_stylesheets=[dbc.themes.YETI]
 )
 app.title = 'Client Segmentation Dashboard'
+
+
+
+df_clustered = None
+def get_df_for_cluster_for_plots():
+    global df_clustered
+    if df_clustered is None:
+        df_clustered = pd.read_csv('df_clustered.csv')
+    return df_clustered
+
+
+def generate_cluster_histogram():
+    fig = px.bar(
+        get_df_for_cluster_for_plots()['cluster'].value_counts(),
+        y='cluster',
+        labels={'index': 'cluster', 'cluster': '#'}
+    )
+    return fig
+
+
+def generate_cluster_3dscatter():
+    fig = px.scatter_3d(
+        get_df_for_cluster_for_plots(),
+        x='Income',
+        y='NumAllPurchases',
+        z='AverageCheck',
+        color='cluster'
+    )
+    fig.update_layout(height=800)
+    return fig
 
 
 def build_banner():
@@ -55,23 +91,45 @@ def build_tab_1():
         ]
     )
 
+
 def build_tab_2():
-    return html.Div(
-        id='tab-2',
-        children=[
-            html.P("Tab 2")
+    return dbc.Row(
+        [
+            dbc.Col(
+                [
+                    html.B('Cluster Histogram'),
+                    html.Hr(),
+                    dcc.Graph(id='cluster_histogram', figure=generate_cluster_histogram())
+                ],
+                width=6
+            ),
+            dbc.Col(
+                [
+                    html.B('3D Scatter'),
+                    html.Hr(),
+                    dcc.Graph(id='cluster_3dscatter', figure=generate_cluster_3dscatter())
+                ],
+                width=6
+            )
         ]
     )
 
 
-app.layout = html.Div(
-    id='big-app-container',
-    children=[
+
+
+
+
+
+app.layout = dbc.Container(
+    [
         build_banner(),
         build_tabs(),
         html.Div(id='app-content')
     ],
+    className='dbc',
+    fluid=True
 )
+
 
 @app.callback(
     Output('app-content', 'children'),
@@ -84,6 +142,9 @@ def update_tabs(tab):
         return build_tab_2()
     else:
         raise ValueError(tab)
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8736)
