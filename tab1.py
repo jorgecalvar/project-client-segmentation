a2ddd08b3bbe0eaf58510716326771adff3181e2
fersantanag <fer.sg2000@gmail.com>
2022-12-06 14:33:33 +0100

    add

--- tab1.py
+++ tab1.py
@@ -57,12 +57,34 @@ def build_tab_1():
                 [
                     dbc.Col(
                         [
+                            dbc.Row(  # Primera fila
+                                [
+                                html.P("""
+                                Una empresa ha recopilado datos sobre sus clientes y las compras que han realizado últimamente, con el fin de crear productos personalizados o hacer campañas 
+                                de marketing dirigidas quieren buscar perfiles de clientes que se puedan parecer entre ellos. Con estos datos la empresa os pide:"""),
+                                html.Ul([
+                                    html.Li("Realizar un análisis descriptivo de los datos buscando patrones comunes entre clientes"),
+                                    html.Li("Realizar un análisis descriptivo de los datos buscando patrones comunes entre clientes"),
+                                    html.Li("Realizar un análisis descriptivo de los datos buscando patrones comunes entre clientes")
+                                ])
 
+                                ]
+                            ),
+                            dbc.Row(  # Primera fila
+                                [
+                                html.P("hello"
+
+                                       ),
+                                ]
+                            ),
+                            dbc.Row(  # Primera fila
+                                dcc.Graph(id='figure_2')
+                            ),
                         ],
                         width=4
                     ),
 
-                    dbc.Col(  # Bloque izquierdo
+                    dbc.Col(  # Bloque derecho
                         [
                             dbc.Row(
                                 [
@@ -301,139 +323,7 @@ def build_tab_1():
                         className='p-4'
                     ),
 
-                    # """
-                    # dbc.Col(  # Bloque Derecho
-                    #     children=[
-                    #         html.Div(  # filtros izquierda
-                    #             children=[
-                    #                 dcc.Dropdown(
-                    #                     options=options_dropdown_status,
-                    #                     placeholder="Marital Status",
-                    #                     id="status_dropdown_2"
-                    #                 ),
-                    #                 dcc.Dropdown(
-                    #                     options=options_dropdown_relation,
-                    #                     placeholder="Relation ",
-                    #                     id="relation_dropdown_2"
-                    #                 ),
-                    #                 dcc.Dropdown(
-                    #                     options=options_dropdown_education,
-                    #                     placeholder="Education",
-                    #                     id="education_dropdown_2"
-                    #                 ),
-                    #                 dcc.Dropdown(
-                    #                     options=options_dropdown_degree,
-                    #                     placeholder="Degree",
-                    #                     id="degree_dropdown_2"
-                    #                 ),
-                    #             ],
-                    #             style={
-                    #                 "width": "17%", "display": "inline-block",
-                    #             },
-                    #         ),
-                    #         html.Div(  # labels centro
-                    #             children=[
-                    #                 html.H5("age"),
-                    #                 html.H5("days"),
-                    #                 html.H5("income$"),
-                    #                 html.H5("spent$"),
-                    #             ],
-                    #             style={
-                    #                 "width": "8%", "display": "inline-block", 'vertical-align': 'top'
-                    #             },
-                    #         ),
-                    #         html.Div(  # filtros centro
-                    #             children=[
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.Age), max=max(df_selectedvariables.Age),
-                    #                     value=[min(df_selectedvariables.Age), max(df_selectedvariables.Age)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='Age_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.days_enrolled),
-                    #                     max=max(df_selectedvariables.days_enrolled),
-                    #                     value=[min(df_selectedvariables.days_enrolled),
-                    #                            max(df_selectedvariables.days_enrolled)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='days_enrolled_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.Income), max=max(df_selectedvariables.Income),
-                    #                     value=[min(df_selectedvariables.Income), max(df_selectedvariables.Income)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='Income_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.Total_Spent),
-                    #                     max=max(df_selectedvariables.Total_Spent),
-                    #                     value=[min(df_selectedvariables.Total_Spent),
-                    #                            max(df_selectedvariables.Total_Spent)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='Total_Spent_slider_2'),
-                    #             ],
-                    #             style={
-                    #                 "width": "30%", "display": "inline-block",
-                    #             },
-                    #         ),
-                    #         html.Div(  # labels centro
-                    #             children=[
-                    #                 html.H5("Num_purch"),
-                    #                 html.H5("Avg_Check$"),
-                    #                 html.H5("Deal_Purch%"),
-                    #                 html.H5("kids_home"),
-                    #             ],
-                    #             style={
-                    #                 "width": "12%", "display": "inline-block", 'vertical-align': 'top'
-                    #             },
-                    #         ),
-                    #         html.Div(  # filtros derecha
-                    #             children=[
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.NumAllPurchases),
-                    #                     max=max(df_selectedvariables.NumAllPurchases),
-                    #                     value=[min(df_selectedvariables.NumAllPurchases),
-                    #                            max(df_selectedvariables.NumAllPurchases)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='NumAllPurchases_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.AverageCheck),
-                    #                     max=max(df_selectedvariables.AverageCheck),
-                    #                     value=[min(df_selectedvariables.AverageCheck),
-                    #                            max(df_selectedvariables.AverageCheck)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='AverageCheck_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.ShareDealsPurchases),
-                    #                     max=max(df_selectedvariables.ShareDealsPurchases),
-                    #                     value=[min(df_selectedvariables.ShareDealsPurchases),
-                    #                            max(df_selectedvariables.ShareDealsPurchases)],
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='ShareDealsPurchases_slider_2'),
-                    #                 dcc.RangeSlider(
-                    #                     min=min(df_selectedvariables.kidsteenHome),
-                    #                     max=max(df_selectedvariables.kidsteenHome),
-                    #                     value=[min(df_selectedvariables.kidsteenHome),
-                    #                            max(df_selectedvariables.kidsteenHome)],
-                    #                     step=1,
-                    #                     tooltip={"placement": "bottom", "always_visible": True},
-                    #                     id='kidsteenHome_slider_2'),
-                    #             ],
-                    #             style={
-                    #                 "width": "29%", "display": "inline-block",
-                    #             },
-                    #         ),
-                    #         dcc.Graph(
-                    #             id="figure_2",
-                    #             style={
-                    #                 "display": "none"
-                    #             }
-                    #         )
-                    #     ],
-                    #     style={
-                    #         "border-left": "solid gray",
-                    #     },
-                    #     width=6,
-                    #     className='p-4'
-                    # ),
-                    # """
+
                 ]
             ),
             html.Hr(
@@ -572,7 +462,6 @@ def build_tab_1():
                     ),
                 ]
             ),
-
         ]
     )
 
@@ -652,7 +541,8 @@ def create_callbacks_for_tab1():
         return (fig, {"display": "block"})
 
 
-    """
+
+
     @app.callback(
         Output("figure_2", "figure"),
         Output("figure_2", "style"),
@@ -671,37 +561,79 @@ def create_callbacks_for_tab1():
     )
     def figure2(stat, rel, edu, deg, ag, day, inco, spen, purch, check, share, kids):
 
-        dff = df_selectedvariables[(df_selectedvariables.Age >= ag[0]) & (df_selectedvariables.Age <= ag[0])]
+        df = df_selectedvariables
 
-        df_num = df_selectedvariables.iloc[:, 5:14]
-        features = [x for x in df_num.columns]
+        print(stat)
 
-        fig = make_subplots(rows=3,
-                            cols=3,
-                            subplot_titles=features)
+        dff = df.loc[
+            (df.Age >= ag[0]) & (df.Age <= ag[1]) & # Age
+            (df.days_enrolled >= day[0]) & (df.days_enrolled <= day[1]) & # Days enrolled
+            (df.Income >= inco[0]) & (df.Income <= inco[1]) & # Income
+            (df.Total_Spent >= spen[0]) & (df.Total_Spent <= spen[1]) & # Total Spent
+            (df.NumAllPurchases >= purch[0]) & (df.NumAllPurchases <= purch[1]) & # Numm All purchases
+            (df.AverageCheck >= check[0]) & (df.AverageCheck <= check[1]) & # Average check
+            (df.ShareDealsPurchases >= share[0]) & (df.ShareDealsPurchases <= share[1]) & # Share deals
+            (df.kidsteenHome >= kids[0]) & (df.kidsteenHome <= kids[1]) # Kids
+            ]
+        if stat is not None:
+            dff = dff.loc[dff.Marital_Status == stat]
+        if rel is not None:
+            dff = dff.loc[dff.Relationship == rel]
+        if edu is not None:
+            dff = dff.loc[dff.Education == edu]
+        if deg is not None:
+            dff = dff.loc[dff.GradorPost == deg]
 
-        # Preparo la estructura de filas y columnas para asignar
-        rows = np.repeat([1, 2, 3], 3)
-        cols = [1, 2, 3] * 3
+        df_cat=dff.iloc[:, 1:5]
 
-        # Creo secuencialmente la figura con un loop
-        for feature, row, col in zip(features, rows, cols):
-            if feature == features[-1]:
-                showlegend = True
-            else:
-                showlegend = False
-            fig.add_trace(
-                go.Histogram(
-                    x=df_num[feature],
-                    name=feature, showlegend=False
-                ),
-                row=row,
-                col=col
-            )
-        fig.update_layout(title="Histograms ", bargap=0.1, barmode="overlay",
-                          height=600)
+        fig = make_subplots(rows=2,
+                            cols=2,
+                            specs=[[{"type": "pie"}, {"type": "pie"}],[{"type": "pie"}, {"type": "pie"}]],
+                            subplot_titles=["In a rellation or not", "Grad or PostGrad Degree", "Type of Relationship", "Specific Education"])
+
+        fig.add_trace(
+            go.Pie(
+                labels=df_cat.Relationship.value_counts().index.tolist(),
+                values=df_cat.Relationship.value_counts().tolist(),
+                textinfo='label+percent', insidetextorientation='radial', hole=.4
+            ),
+            row=1,
+            col=1
+        )
+        fig.add_trace(
+            go.Pie(
+                labels=df_cat.GradorPost.value_counts().index.tolist(),
+                values=df_cat.GradorPost.value_counts().tolist(),
+                textinfo='label+percent', insidetextorientation='radial',hole=.4
+
+            ),
+            row=2,
+            col=1
+        )
+        fig.add_trace(
+            go.Pie(
+                labels=df_cat.Marital_Status.value_counts().index.tolist(),
+                values=df_cat.Marital_Status.value_counts().tolist(),
+                textinfo='label+percent', insidetextorientation='radial'
+            ),
+            row=1,
+            col=2
+        )
+        fig.add_trace(
+            go.Pie(
+                labels=df_cat.Education.value_counts().index.tolist(),
+                values=df_cat.Education.value_counts().tolist(),
+                textinfo='label+percent', insidetextorientation='radial'
+
+            ),
+            row=2,
+            col=2
+        )
+        fig.update_layout()
         return (fig, {"display": "block"})
-    """
+
+
+
 
 
     @app.callback(
